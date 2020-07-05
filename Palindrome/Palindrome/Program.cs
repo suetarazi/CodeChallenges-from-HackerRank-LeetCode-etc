666cb0bae0b92f09a97c364230d96f035b42c4fa
Sue Tarazi <suemachtley@gmail.com>
2020-07-05 15:32:57 -0700

    palindrome - initial code

---
+++ Palindrome/Palindrome/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+
+namespace Palindrome
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public class Solution
+        {
+            public bool IsPalindrome(int x)
+            {
+                string stringInput = x.ToString();
+                char[] charInput = stringInput.ToCharArray();
+
+                char[] resultArray = new char[charInput.Length];
+                resultsArray = Array.Reverse(charInput);
+
+                if (charInput == resultsArray)
+                {
+                    return true;
+                }
+                else
+                    return false;
+                                                
+            }
+        }
+    }
+}
