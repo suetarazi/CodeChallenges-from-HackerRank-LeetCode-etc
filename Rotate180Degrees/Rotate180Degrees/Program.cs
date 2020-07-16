9442814ca7a1fcb71b05968115c3fa6f39bf7957
Sue Tarazi <suemachtley@gmail.com>
2020-07-16 16:34:34 -0700

    rotate 180 code

---
+++ Rotate180Degrees/Rotate180Degrees/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Rotate180Degrees
+{
+    
+        public class Program
+        {
+            static void Main(string[] args)
+            {
+                Console.WriteLine("Hello World!");
+                Solution s = new Solution();
+                int N = 34563;
+                int count = s.RotatedDigits(N);
+                Console.WriteLine(count);
+            }
+        }
+
+        public class Solution
+        {
+            public int RotatedDigits(int N)
+            {
+                string strN = N.ToString();
+                char[] charN = strN.ToCharArray();
+                int count = 0;
+                foreach (char n in charN)
+
+                {
+                    if (N == 2)
+                    {
+                        N = 5;
+                        count++;
+                    }
+                    else if (N == 5)
+                    {
+                        N = 2;
+                        count++;
+                    }
+                    else if (N == 6)
+                    {
+                        N = 9;
+                        count++;
+                    }
+                    else if (N == 9)
+                    {
+                        N = 6;
+                        count++;
+                    }
+                    else;
+                }
+                return count;
+            }
+        }
+    }
+
