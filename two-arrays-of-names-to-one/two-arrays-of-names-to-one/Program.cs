6f459af23ad76b95a2b519d2c6187978dfece45c
Sue Tarazi <suemachtley@gmail.com>
2020-07-17 12:15:27 -0700

    merge two arrays of names

---
+++ two-arrays-of-names-to-one/two-arrays-of-names-to-one/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace two_arrays_of_names_to_one
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string[] names1 = new string[] { "Ava", "Emma", "Olivia" };
+            string[] names2 = new string[] { "Olivia", "Sophia", "Emma" };
+            Console.WriteLine(string.Join(", ", MergeNames.UniqueNames(names1, names2))); // should print Ava, Emma, Olivia, Sophia
+        }
+    }
+    public class MergeNames
+    {
+        public static string[] UniqueNames(string[] names1, string[] names2)
+        {
+            throw new NotImplementedException();
+            Hashtable hashtable = new Hashtable();
+            
+            for(int i=0; i<names1.Length; i++)
+            {
+                hashtable.Add(i, names1[i]);
+            }
+
+            List<string> resultList = new List<string>();
+            for(int i=0; i<names2.Length; i++)
+            {
+                if (hashtable.Contains(names2[i]))
+                {
+                    //do nothing
+                }
+                else
+                {
+                    hashtable.Add(i, names2[i]);
+                }
+            }
+            foreach (string name in hashtable)
+            {
+                resultList.Add(name);
+            }
+
+            return resultList.ToArray();
+
+        }
+
+    }
+}
