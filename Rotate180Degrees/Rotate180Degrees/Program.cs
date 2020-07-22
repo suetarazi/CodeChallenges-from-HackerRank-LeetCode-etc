00da1a6de1968e723cf286a91f5bdb099c9018ff
Sue Tarazi <suemachtley@gmail.com>
2020-07-21 17:11:07 -0700

    added try catch block

--- Rotate180Degrees/Rotate180Degrees/Program.cs
+++ Rotate180Degrees/Rotate180Degrees/Program.cs
@@ -1,54 +1,74 @@
 ﻿using System;
+using System.IO;
 
 namespace Rotate180Degrees
 {
-    
-        public class Program
-        {
-            static void Main(string[] args)
-            {
-                Console.WriteLine("Hello World!");
-                Solution s = new Solution();
-                int N = 34563;
-                int count = s.RotatedDigits(N);
-                Console.WriteLine(count);
-            }
-        }
 
-        public class Solution
+    public class Program
+    {
+        static void Main(string[] args)
         {
-            public int RotatedDigits(int N)
-            {
-                string strN = N.ToString();
-                char[] charN = strN.ToCharArray();
-                int count = 0;
-                foreach (char n in charN)
-
-                {
-                    if (N == 2)
-                    {
-                        N = 5;
-                        count++;
-                    }
-                    else if (N == 5)
-                    {
-                        N = 2;
-                        count++;
-                    }
-                    else if (N == 6)
-                    {
-                        N = 9;
-                        count++;
-                    }
-                    else if (N == 9)
-                    {
-                        N = 6;
-                        count++;
-                    }
-                    else;
-                }
-                return count;
-            }
+            Console.WriteLine("Hello World!");
+            Solution s = new Solution();
+            int N = 34563;
+            int count = s.RotatedDigits(N);
+            Console.WriteLine(count);
         }
     }
+
+    public class Solution
+    {
+        public int RotatedDigits(int N)
+        {
+            string strN = N.ToString();
+            //Console.WriteLine($"strN is {strN}");
+            char[] charN = strN.ToCharArray();
+            int count = 0;
+            int totalCount = 0;
+            for (int i = 0; i < charN.Length; i++)
+            //Console.WriteLine($"charN characters are {n}");
+            {
+                try
+                {
+                    if (charN[i] == 2)
+                    {
+                        charN[i] = '5';
+                        count++;
+                        totalCount += count;
+                    }
+                    else if (charN[i] == 5)
+                    {
+                        Console.WriteLine($"i is {i} and charN[i] is {charN[i]}");
+                        charN[i] = '2';
+                        count++;
+                        totalCount += count;
+                    }
+                    else if (charN[i] == 6)
+                    {
+                        Console.WriteLine($"i is {i} and charN[i] is {charN[i]}");
+                        charN[i] = '9';
+                        count++;
+                        totalCount += count;
+                    }
+                    else if (charN[i] == 9)
+                    {
+                        Console.WriteLine($"i is {i} and charN[i] is {charN[i]}");
+                        charN[i] = '6';
+                        count++;
+                        totalCount += count;
+                    }
+                Console.WriteLine($"the total count is {totalCount}");
+                Console.WriteLine($"count is {count}");
+                return count;
+                    
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("No results found", ex.Message);
+                }
+            }
+                    return count;
+        }
+    }
+}
 
