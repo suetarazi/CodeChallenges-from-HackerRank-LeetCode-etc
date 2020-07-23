e1e0e1ddb9d54632180f53bc1d532a44671c3641
Sue Tarazi <suemachtley@gmail.com>
2020-07-23 15:50:09 -0700

    switch statement

--- Rotate180Degrees/Rotate180Degrees/Program.cs
+++ Rotate180Degrees/Rotate180Degrees/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Data;
 using System.IO;
 
 namespace Rotate180Degrees
@@ -21,56 +22,83 @@ namespace Rotate180Degrees
         public int RotatedDigits(int N)
         {
             string strN = N.ToString();
+            string result = " ";
             //Console.WriteLine($"strN is {strN}");
-            char[] charN = strN.ToCharArray();
+            foreach (char c in strN)
+            {
+                result = += Map(c);
+            }
+
+            return int.Parse(result);
+
             int count = 0;
             int totalCount = 0;
 
-            for (int i = 0; i < charN.Length; i++)
-            //Console.WriteLine($"charN characters are {n}");
+        }
+
+        public string Map(char c)
+        {
+            switch(c)
             {
-                //try
-                //{
-                    if (charN[i] == 2)
-                    {
-                        charN[i] = '5';
-                        Console.WriteLine($"i is {i} and charN[i] is {charN[i]}");
-                        count++;
-                        totalCount += count;
-                    }
-                    else if (charN[i] == 5)
-                    {
-                        charN[i] = '2';
-                        Console.WriteLine($"i is {i} and charN[i] is {charN[i]}");
-                        count++;
-                        totalCount += count;
-                    }
-                    else if (charN[i] == 6)
-                    {
-                        charN[i] = '9';
-                        Console.WriteLine($"i is {i} and charN[i] is {charN[i]}");
-                        count++;
-                        totalCount += count;
-                    }
-                    else if (charN[i] == 9)
-                    {
-                        charN[i] = '6';
-                        Console.WriteLine($"i is {i} and charN[i] is {charN[i]}");
-                        count++;
-                        totalCount += count;
-                    }
-                Console.WriteLine($"the total count is {totalCount}");
-                Console.WriteLine($"count is {count}");
-                return totalCount;
+                case '2':
+                    c = '5';
+                    break;
+                    
+                case '5':
+                    c = '2';
+                    break;
+                case '6':
+                    c = '9';
+                    break;
+                case '9':
+                    c = '6';
+                    break;
+                default:
+                    c = c;
+                    break;
+            }
+            return c.ToString();
+        }
+        //            if (charN[i] == 2)
+        //            {
+        //                charN[i] = '5';
+        //                Console.WriteLine($"i is {i} and charN[i] is {charN[i]}");
+        //                count++;
+        //                totalCount += count;
+        //            }
+        //            else if (charN[i] == 5)
+        //            {
+        //                charN[i] = '2';
+        //                Console.WriteLine($"i is {i} and charN[i] is {charN[i]}");
+        //                count++;
+        //                totalCount += count;
+        //            }
+        //            else if (charN[i] == 6)
+        //            {
+        //                charN[i] = '9';
+        //                Console.WriteLine($"i is {i} and charN[i] is {charN[i]}");
+        //                count++;
+        //                totalCount += count;
+        //            }
+        //            else if (charN[i] == 9)
+        //            {
+        //                charN[i] = '6';
+        //                Console.WriteLine($"i is {i} and charN[i] is {charN[i]}");
+        //                count++;
+        //                totalCount += count;
+        //            }
+        //        Console.WriteLine($"the total count is {totalCount}");
+        //        Console.WriteLine($"count is {count}");
+        //        return totalCount;
                     
                 
-                //catch (Exception ex)
-                //{
-                //    Console.WriteLine("No results found", ex.Message);
-                //}
-            }
-            return count;
-        }
+        //        //catch (Exception ex)
+        //        //{
+        //        //    Console.WriteLine("No results found", ex.Message);
+        //        //}
+        //    }
+        //    return count;
+        //}
     }
 }
 
