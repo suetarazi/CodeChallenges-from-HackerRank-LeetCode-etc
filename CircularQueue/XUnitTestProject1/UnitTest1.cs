1e76ed95efabafefe30c2db251e328afb84e4123
Sue Tarazi <suemachtley@gmail.com>
2020-07-29 16:57:26 -0700

    added XUnit

---
+++ CircularQueue/XUnitTestProject1/UnitTest1.cs
@@ -0,0 +1,17 @@
+using System;
+using Xunit;
+using CircularQueue;
+
+
+namespace XUnitTestProject1
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void EmptyQueueTest()
+        {
+            isCount = 0;
+
+        }
+    }
+}
