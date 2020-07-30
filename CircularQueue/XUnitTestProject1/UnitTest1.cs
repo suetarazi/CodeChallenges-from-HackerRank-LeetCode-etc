23cbcce257aeab4a9b434fc03774bab86c10a729
Sue Tarazi <suemachtley@gmail.com>
2020-07-30 13:02:20 -0700

    Assert.Throws exception - still not working

--- CircularQueue/XUnitTestProject1/UnitTest1.cs
+++ CircularQueue/XUnitTestProject1/UnitTest1.cs
@@ -1,22 +1,33 @@
 using System;
 using Xunit;
 using CircularQueue;
-
+using System.Collections;
+using System.Collections.Generic;
 
 namespace XUnitTestProject1
 {
-    public class UnitTest1
+    public class UnitTest1 : IEnumerable
     {
         [Fact]
         public void EmptyQueueTest()
         {
             CircularBuffer<int> testData = new CircularBuffer<int>(16);
             //is.Count = 0;
-            _rear + 1 == _front;
-            testData.Count = 0;
-            var actual = testData.Dequeue();
-            var expected = "The buffer is empty";
-            Assert.Equal(expected, actual);
+            //_rear + 1 == _front;
+            //testData.Count = 0;
+            //var actual = testData.Dequeue();
+            //var expected = "The buffer is empty";
+            Assert.Throws<InvalidOperationException>(() => testData.Dequeue());
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
         }
     }
 }
