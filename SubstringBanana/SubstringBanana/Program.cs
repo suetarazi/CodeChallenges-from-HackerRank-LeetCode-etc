48e19bc65bf9eeb23fbc04037ec0a08febe1cd9f
Sue Tarazi <suemachtley@gmail.com>
2020-08-17 11:33:54 -0700

    test driven development - XUnit test created

---
+++ SubstringBanana/SubstringBanana/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SubstringBanana
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+    public string SubstringB(string s)
+    {
+        char[] charArr = s.ToCharArray();
+        List<string> list = new List<string>();
+
+
+
+
+        return list.Last();
+    }
+}
+}
