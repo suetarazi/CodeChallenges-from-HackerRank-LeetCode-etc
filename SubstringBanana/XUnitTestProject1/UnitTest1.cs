31ec54ac9c237f0b24aa6518c8d114ebaf35e915
Sue Tarazi <suemachtley@gmail.com>
2020-08-17 12:29:34 -0700

    code working; added conditional for empty string

--- SubstringBanana/XUnitTestProject1/UnitTest1.cs
+++ SubstringBanana/XUnitTestProject1/UnitTest1.cs
@@ -14,7 +14,37 @@ namespace XUnitTestProject1
             string s = "banana";
             Program p = new Program();
             string actual = p.SubstringB(s);
-            string expected = "na";
+            string expected = "nana";
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void SubstringOneLetterTest()
+        {
+            string s = "l";
+            Program p = new Program();
+            string actual = p.SubstringB(s);
+            string expected = "l";
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void SubstringEmptyTest()
+        {
+            string s = " ";
+            Program p = new Program();
+            string actual = p.SubstringB(s);
+            Assert.null(actual);
+
+        }
+
+        [Fact]
+        public void SubstringLongTest()
+        {
+            string s = "AlyaZadeAveryMarco";
+            Program p = new Program();
+            string actual = p.SubstringB(s);
+            string expected = "ZadeAveryMarco";
             Assert.Equal(expected, actual);
         }
     }
