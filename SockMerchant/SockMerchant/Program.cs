a26fd14855611fbc3250e8d3bb273110a6e8ceca
Sue Tarazi <suemachtley@gmail.com>
2020-08-18 12:02:01 -0700

    created a helper method

--- SockMerchant/SockMerchant/Program.cs
+++ SockMerchant/SockMerchant/Program.cs
@@ -10,24 +10,43 @@ namespace SockMerchant
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            int n = 9;
+            int[] testArr = new int[] { 10, 20, 20, 10, 10, 30, 50, 10, 20 };
+            Program p = new Program();
+            p.FindMatchingPairsOfSocks(n, testArr);
         }
 
         public int FindMatchingPairsOfSocks(int n, int[] arr)
         {
             Array.Sort(arr);
-            HashSet<int> hashset = new HashSet<int>();
-            int matchingPairs = 0;
-            hashset.Add(arr[0]);
+            int matchingPairs=0;
+            
             for(int i = 0; i < arr.Length; i++)
             {
-                if(hashset.Contains(i))
-                {
-                    hashset.Add(i);
-                    matchingPairs++;
-                }
-            }
-            
+                helperMethod(arr[i]);
+
             return matchingPairs;
+            }
+
+            return -1;
+        }
+
+        public void helperMethod(int i)
+        {
+            int matchingPairs = 0;
+            HashSet<int> hashset = new HashSet<int>();
+            if (!hashset.Contains(i))
+            {
+                hashset.Add(i);
+                Console.WriteLine($"Initially added {i}");
+
+            }
+            else if (hashset.Contains(i))
+            {
+                matchingPairs++;
+                Console.WriteLine($"We found a pair {i}");
+                hashset.Remove(i);
+            }
         }
 
     }
