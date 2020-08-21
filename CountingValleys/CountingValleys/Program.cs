60a8eaee2df4cac32412a03ddcbecd9415777da4
Sue Tarazi <suemachtley@gmail.com>
2020-08-21 16:33:15 -0700

    created project

---
+++ CountingValleys/CountingValleys/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace CountingValleys
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+
+    }
+}
