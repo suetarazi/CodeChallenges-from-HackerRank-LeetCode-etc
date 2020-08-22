5d82c29322cc3c49886ed958850d5c2b5a6b7987
Sue Tarazi <suemachtley@gmail.com>
2020-08-21 17:13:23 -0700

    strategy: assign 1 to U, -1 to D. Count the number of times the string goes from + to -.

--- CountingValleys/CountingValleys/Program.cs
+++ CountingValleys/CountingValleys/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace CountingValleys
 {
@@ -11,8 +12,20 @@ namespace CountingValleys
 
         public int countingValleys(int n, string s)
         {
+            //if U = 1 and D = -1; count the number of times the string changes from + to -
+            int valleys = 0;
+            int U = 1;
+            int D = -1; 
+            char[] arr = s.ToCharArray();
+            for(char c=arr[0]; c<n; c++)
+            {
+                U = 1;
+                D = -1;
+                
+            }
 
-            return -1;
+
+            return valleys;
         }
 
     }
