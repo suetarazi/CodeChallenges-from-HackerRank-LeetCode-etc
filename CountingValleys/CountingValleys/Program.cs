f193648d4f5684a707c17c8620e83d961b654f87
Sue Tarazi <suemachtley@gmail.com>
2020-08-21 17:24:55 -0700

    initial code

--- CountingValleys/CountingValleys/Program.cs
+++ CountingValleys/CountingValleys/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 
 namespace CountingValleys
@@ -14,16 +15,31 @@ namespace CountingValleys
         {
             //if U = 1 and D = -1; count the number of times the string changes from + to -
             int valleys = 0;
-            int U = 1;
-            int D = -1; 
+            //int U = 1;
+            //int D = -1; 
             char[] arr = s.ToCharArray();
+            int[] numArr = new int[n];
             for(char c=arr[0]; c<n; c++)
             {
-                U = 1;
-                D = -1;
-                
-            }
+                if(arr[c] == 'U')
+                {
+                    numArr[c] = 1;
+                }
+                else
+                {
+                    numArr[c] = -1;
+                }
 
+            }
+            foreach(int i in numArr)
+            {
+                int sum = 0;
+                if(sum < 0)
+                {
+                    valleys++;
+                }
+                sum += numArr[i];
+            }
 
             return valleys;
         }
