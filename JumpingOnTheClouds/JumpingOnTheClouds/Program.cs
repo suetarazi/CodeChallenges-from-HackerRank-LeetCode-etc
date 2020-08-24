a12f0b2cebed4a2174e6a17acf18a16e13ac06da
Sue Tarazi <suemachtley@gmail.com>
2020-08-24 15:34:32 -0700

    one test passing

--- JumpingOnTheClouds/JumpingOnTheClouds/Program.cs
+++ JumpingOnTheClouds/JumpingOnTheClouds/Program.cs
@@ -7,28 +7,45 @@ namespace JumpingOnTheClouds
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Program p = new Program();
+            int[] arr = new int[] { 0, 0, 1, 0, 0, 1, 0 };
+            int[] arr2 = new int[] { 0, 0, 0, 1, 0, 0 };
+            p.jumpingOnClouds(arr2);
         }
 
         public int jumpingOnClouds(int[] c)
         {
+            //[0, 0, 1, 0, 0, 1, 0]
             int totalSteps = 0;
-            foreach(int i in c)
+            int i = 0;
+            while(i<c.Length-1)
             {
-                if(c[i]==1)
+                if(c[0]==1)
                 {
-                    return 0;
+                    Console.WriteLine("You are starting with 1");
                 }
-                else if(c[i]==0 && c[i+2]==0)
+                else if(c[i+1]==0 && c[i+2]<=c.Length-1 && c[i + 2] == 0)
                 {
                     totalSteps++;
-                    totalSteps++;
+                    i++;
+                    i++;
+                    Console.WriteLine($"adding two steps yields {totalSteps}");
                 }
-                else if(c[i]==0 && c[i+1]==0)
+                else if(c[i+1]==0 && c[i+2]!=0)
                 {
                     totalSteps++;
+                    i++;
+                    Console.WriteLine($"adding one step yields {totalSteps}");
+                }
+                else if(c[i+1]!=0 && c[i+2]==0)
+                {
+                    totalSteps++;
+                    i++;
+                    i++;
                 }
             }
 
+            Console.WriteLine($"The total number of steps is {totalSteps}");
             return totalSteps;
 
         }
