35b335cd48bbf4f73f50d671dc0efcc39f01cdda
Sue Tarazi <suemachtley@gmail.com>
2020-08-25 15:04:51 -0700

    initial scaffolding

---
+++ RepeatedString/RepeatedString/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace RepeatedString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        static long repeatedString(string s, long n)
+        {
+            long occurancesOfA = 0;
+
+
+            return occurancesOfA;
+        }
+    }
+}
