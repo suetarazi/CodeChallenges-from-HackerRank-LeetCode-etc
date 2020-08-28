6d44477e2c4416f49e511f42841ee4887f386fc4
Sue Tarazi <suemachtley@gmail.com>
2020-08-27 21:42:11 -0700

    code working and all tests passing!

--- RepeatedString/RepeatedString/Program.cs
+++ RepeatedString/RepeatedString/Program.cs
@@ -17,25 +17,59 @@ namespace RepeatedString
 
         public static long CountRepeatedString(string s, long n)
         {
-            
-            long arrLength = s.ToArray().Length;
-            string[] inputArr = Enumerable.Repeat("s", Convert.ToInt32(n)).ToArray();
+            //abaabaabaa
+            //aba 
+            //1. count "a"s in string (result is 2)
+            //2. determine arrayLooping variable
+            //3. determine remainderLoop variable
+            //4. totalCount = (count * arrayLooping) + (forloop of k)
 
-            char[] inputChar = inputArr.ToCharArray();
-            foreach (char c in inputChar)
+            long count = 0;
+            long arrayLooping = n/(s.Length);
+            long remainderLoop = n % s.Length;
+
+            for(int i=0; i<arrayLooping; i++)
             {
-                Console.WriteLine();
-            }
-            long occurancesOfA = 0;
-            for(int i=0; i<inputChar.Length; i++)
-            {
-                if (inputArr[i] == "a")
+                for(int j=0; j<s.Length; j++)
                 {
-                    occurancesOfA++;
+                    if(s[j]=='a')
+                    {
+                        count++;
+                    }
                 }
             }
+            for(int k=0; k<remainderLoop; k++)
+            {
+                if(s[k]=='a')
+                {
+                    count++;
+                }
+            }
+            Console.WriteLine($"the total count is {count}");
+            return count;
+            
+            //long arrLength = s.ToArray().Length;
+            //string[] inputArr = Enumerable.Repeat(s, Convert.ToInt32(n)).ToArray();
+            //foreach(string st in inputArr)
+            //{
+            //    Console.WriteLine($"{st}");
+            //}
 
-            return occurancesOfA;
+            //char[] inputChar = inputArr.ToCharArray();
+            //foreach (char c in inputChar)
+            //{
+            //    Console.WriteLine();
+            //}
+            long occurancesOfA = 0;
+            //for(int i=0; i<inputChar.Length; i++)
+            //{
+            //    if (inputArr[i] == "a")
+            //    {
+            //        occurancesOfA++;
+            //    }
+            //}
+
+            //return occurancesOfA;
         }
     }
 }
