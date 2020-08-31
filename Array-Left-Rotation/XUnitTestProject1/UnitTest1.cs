dd9203db6caecccfb78aa1a0dfb37fa8fb92f707
Sue Tarazi <suemachtley@gmail.com>
2020-08-31 15:11:21 -0700

    scaffolding and XUnit tests for TDD done

---
+++ Array-Left-Rotation/XUnitTestProject1/UnitTest1.cs
@@ -0,0 +1,24 @@
+using Microsoft.VisualStudio.TestPlatform.TestHost;
+using System;
+using Xunit;
+using Array_Left_Rotation;
+using Program = Array_Left_Rotation.Program;
+
+namespace XUnitTestProject1
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            int testn = 5;
+            int testd = 4;
+            int[] testInput = new int[] { 1, 2, 3, 4, 5 };
+            Program p = new Program();
+            int[] actual = p.LeftRotation(testn, testd, testInput);
+            int[] expected = new int[] { 5, 1, 2, 3, 4 };
+            Assert.Equal(expected, actual);
+
+        }
+    }
+}
