806170fd714d1e59af0dfbab94d87fa5e78edaf6
Sue Tarazi <suemachtley@gmail.com>
2020-09-03 09:48:54 -0700

    problem domain

---
+++ BonAppetit/BonAppetit/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BonAppetit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        static void bonAppetit(List<int> bill, int k, int b)
+        {
+
+
+        }
+    }
+}
