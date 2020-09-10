54d7164f3769d2a82b94e77bcf6d6bdc4ebe320c
Sue Tarazi <suemachtley@gmail.com>
2020-09-09 21:25:58 -0700

    scaffolding

---
+++ CatsAndAMouse/CatsAndAMouse/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace CatsAndAMouse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        static string catAndMouse(int x, int y, int z)
+        {
+            string winner = " ";
+
+
+
+            return winner;
+        }
+
+    }
+}
