8383b4c1da6dc54cf15468b64a869ecdde42f7db
Sue Tarazi <suemachtley@gmail.com>
2020-09-11 22:04:57 -0700

    initial code

---
+++ InsertANodeAtTheTailOfALinkedList/InsertANodeAtTheTailOfALinkedList/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Net.Http.Headers;
+
+namespace InsertANodeAtTheTailOfALinkedList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        static SinglyLinkedListNode insertNodeAtTail(SinglyLinkedListNode head, int data)
+        
+        while(head != null)
+        {
+            SinglyLinkedListNode node = new SinglyLinkedListNode();
+            node.data = data;
+            Console.WriteLine(node.data);
+            node = head.next;
+            head = node;
+        }
+    }
+}
