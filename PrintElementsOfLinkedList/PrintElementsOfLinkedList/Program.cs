f6da20252f9868dde92c9514f3b14fda47ced996
Sue Tarazi <suemachtley@gmail.com>
2020-09-11 21:50:48 -0700

    passed challenge :)

---
+++ PrintElementsOfLinkedList/PrintElementsOfLinkedList/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace PrintElementsOfLinkedList
+{
+    class Program
+    {
+        
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+              //  For your reference (from HackerRank):
+     
+              //SinglyLinkedListNode {
+              //    int data;
+              //    SinglyLinkedListNode next;
+              //}
+     
+
+        static void printLinkedList(SinglyLinkedListNode head)
+        {
+            while (head != null)
+            {
+
+                Console.WriteLine(head.data);
+                head = head.next;
+            }
+
+
+        }
+    }
