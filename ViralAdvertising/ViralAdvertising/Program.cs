a035e43d870312c1ed13bc41d675914a2f6ab885
Sue Tarazi <suemachtley@gmail.com>
2020-09-28 21:07:48 -0700

    initial scaffold

---
+++ ViralAdvertising/ViralAdvertising/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ViralAdvertising
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+
+        static int viralAdvertising(int n)
+        {
+
+
+            return -1;
+        }
+    }
+}
