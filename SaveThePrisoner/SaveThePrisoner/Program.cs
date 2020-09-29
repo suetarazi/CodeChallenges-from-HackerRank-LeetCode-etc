86486bc393273182e0613bba9cce941059e0add7
Sue Tarazi <suemachtley@gmail.com>
2020-09-28 21:38:33 -0700

    initial scaffolding

---
+++ SaveThePrisoner/SaveThePrisoner/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace SaveThePrisoner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        static int saveThePrisoner(int n, int m, int s)
+        {
+
+
+            return -1;
+        }
+
+    }
+}
