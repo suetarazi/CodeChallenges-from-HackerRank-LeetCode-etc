8561d114fe9ac0901810520e2ad6ee041a446b08
Sue Tarazi <suemachtley@gmail.com>
2020-10-05 12:25:52 -0700

    initial scaffolding and tests done for TDD

---
+++ DesignerPdfViewer/DesignerPdfViewer/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace DesignerPdfViewer
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public int designerPdfViewer(int[] h, string word)
+        {
+
+
+            return -1; 
+
+        }
+    }
+}
