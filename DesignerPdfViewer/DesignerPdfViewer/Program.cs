6ecfded73a775143a4e34ab2d54c47e0cfd14bf3
Sue Tarazi <suemachtley@gmail.com>
2020-10-05 18:03:13 -0700

    Code done! All tests passing :)

--- DesignerPdfViewer/DesignerPdfViewer/Program.cs
+++ DesignerPdfViewer/DesignerPdfViewer/Program.cs
@@ -11,17 +11,33 @@ namespace DesignerPdfViewer
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            int[] inputArr = new int[] { 1, 3, 1, 3, 1, 4, 1, 3, 2, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5 };
+            string inputWord = "abc";
+            Program p = new Program();
+            int result = p.designerPdfViewer(inputArr, inputWord);
+            Console.WriteLine($"The result is {result}");
         }
 
         public int designerPdfViewer(int[] h, string word)
         {
+            //create a dictionary and put the key:value pairs AsciiCodeOfAlphabet:h in the dictionary 
             Dictionary<int, int> dictionary = new Dictionary<int, int>();
-            foreach(int i in h)
+            char[] alphaArr = new char[] { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
+            int[] alphaAsciiArr = new int[26];
+
+            for(int i=0; i<alphaArr.Length; i++)
             {
-                dictionary.Add(h[i], h[i]);
+                alphaAsciiArr[i] = (int)alphaArr[i];
+                Console.WriteLine($"alphaAsciiArr[i] is {alphaAsciiArr[i]}");
+            }
+
+            for(int i=0; i<h.Length; i++)
+            {
+                dictionary.Add(alphaAsciiArr[i], h[i]);
             }
             
-
+            //now take each letter in inputted word, convert each to Ascii to get the Key to look up in the dictionary, then return the value.
 
             int maxHeight = 0;
 
@@ -29,7 +45,8 @@ namespace DesignerPdfViewer
 
             for(int i=0; i<word.Length; i++)
             {
-                wordArr[i] = dictionary.;
+                wordArr[i] = dictionary.GetValueOrDefault((int)word[i]);
+
             }
 
             maxHeight = wordArr.Max<int>();
