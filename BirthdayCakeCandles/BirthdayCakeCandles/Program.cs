b6fd1f629ed22927449947b38218cc7b9bb28647
Sue Tarazi <suemachtley@gmail.com>
2020-10-05 20:48:35 -0700

    Happy code; happy tests :)

--- BirthdayCakeCandles/BirthdayCakeCandles/Program.cs
+++ BirthdayCakeCandles/BirthdayCakeCandles/Program.cs
@@ -8,12 +8,34 @@ namespace BirthdayCakeCandles
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Program p = new Program();
+            List<int> testList = new List<int> { 3, 2, 1, 3 };
+            p.birthdayCakeCandles(testList);
         }
 
         public int birthdayCakeCandles(List<int> candles)
         {
+            int candleCount = 1;
+            candles.Sort();
+            candles.Reverse();
 
-            return -1;
+            for(int i=0; i<candles.Count; i++)
+            {
+                if(candles[i+1] == candles[i])
+                {
+                    candleCount++;
+                }
+
+                else
+                {
+                    
+                    return candleCount;
+                }
+
+            }
+
+
+            return candleCount;
         }
     }
 }
