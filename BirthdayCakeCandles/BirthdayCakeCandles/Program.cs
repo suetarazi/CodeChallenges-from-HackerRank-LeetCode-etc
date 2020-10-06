48f4160f292d8f960e9f672e1cfa8b6336bcabd9
Sue Tarazi <suemachtley@gmail.com>
2020-10-05 20:14:17 -0700

    initial code scaffolded and all tests written for TDD

---
+++ BirthdayCakeCandles/BirthdayCakeCandles/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BirthdayCakeCandles
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public int birthdayCakeCandles(List<int> candles)
+        {
+
+            return -1;
+        }
+    }
+}
