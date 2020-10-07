db10d5c11f725d9114f7143d024141e38dd5a5ec
Sue Tarazi <suemachtley@gmail.com>
2020-10-07 10:22:14 -0700

    Improving BigO for CandleCount by writing a method that only makes ONE pass through the array

--- BirthdayCakeCandles/BirthdayCakeCandles/Program.cs
+++ BirthdayCakeCandles/BirthdayCakeCandles/Program.cs
@@ -13,6 +13,11 @@ namespace BirthdayCakeCandles
             p.birthdayCakeCandles(testList);
         }
 
+        /// <summary>
+        /// This method utilizes built in methods of Sort() and Reverse(), but requires multiple passes through the array. It is therefore O(n) for the Sort() + O(n) for the Reverse() + at worst O(n) for the pass through the array comparing candle heights 
+        /// </summary>
+        /// <param name="candles"></param>
+        /// <returns>int candleCount</returns>
         public int birthdayCakeCandles(List<int> candles)
         {
             int candleCount = 1;
@@ -36,6 +41,33 @@ namespace BirthdayCakeCandles
 
 
             return candleCount;
+        }
+
+        /// <summary>
+        /// This method solves the problem in O(n), only making ONE pass through the array 
+        /// </summary>
+        /// <param name="inputArr"></param>
+        /// <returns>int candleCount</returns>
+        public int CandleCount(int[] inputArr)
+        {
+            int tallestCandle = inputArr[0];
+            int candleCount = 0;
+
+            foreach (int c in inputArr)
+            {
+                if (c > tallestCandle)
+                {
+                    tallestCandle = c;
+                    candleCount = 1;
+
+                }
+                if (c == tallestCandle)
+                {
+                    candleCount++;
+                }
+
+            }
+            return tallestCandle;
         }
     }
 }
