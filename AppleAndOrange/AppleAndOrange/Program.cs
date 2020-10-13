edd894d13d00f6bf1e08715988054a70983d9e78
Sue Tarazi <suemachtley@gmail.com>
2020-10-12 22:29:45 -0700

    apples working; oranges still broken

--- AppleAndOrange/AppleAndOrange/Program.cs
+++ AppleAndOrange/AppleAndOrange/Program.cs
@@ -20,8 +20,25 @@ namespace AppleAndOrange
         /// <param name="oranges">array of oranges and the distances they fall from b</param>
         static void countApplesAndOranges(int s, int t, int a, int b, int[] apples, int[] oranges)
         {
-            
+            int appleCount = 0;
+            int orangeCount = 0;
+            for(int i=0; i<apples.Length; i++)
+            {
+                if(apples[i]>=(s-a) && (apples[i] <= (t-a)))
+                    {
+                    appleCount++;
+                    }
+            }
 
+            for(int j=0; j<oranges.Length; j++)
+            {
+                if(oranges[j]<(b-t) && (oranges[j]>(b-s)))
+                {
+                    orangeCount++;
+                }
+            }
+
+            Console.WriteLine($"{appleCount} {orangeCount}");
         }
     }
 }
