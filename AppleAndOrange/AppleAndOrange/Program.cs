9c7d557e26201e3422fa5b96604021f2b8d8eaff
Sue Tarazi <suemachtley@gmail.com>
2020-10-12 22:01:06 -0700

    initial setup

---
+++ AppleAndOrange/AppleAndOrange/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace AppleAndOrange
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="s">st = width of house</param>
+        /// <param name="t">st = width of house</param>
+        /// <param name="a">distance of apple tree from house</param>
+        /// <param name="b">distance of orange tree from house</param>
+        /// <param name="apples">array of apples and the distances they fall from a</param>
+        /// <param name="oranges">array of oranges and the distances they fall from b</param>
+        static void countApplesAndOranges(int s, int t, int a, int b, int[] apples, int[] oranges)
+        {
+            
+
+        }
+    }
+}
