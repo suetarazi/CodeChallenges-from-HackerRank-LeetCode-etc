3faacbfe4d405847cf3da8cd82d439a32d94d16a
Sue Tarazi <suemachtley@gmail.com>
2020-10-15 12:40:26 -0700

    created project

---
+++ Practice2/Practice2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Practice2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+
+
+    }
+}
